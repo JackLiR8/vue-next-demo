c9cc34fbe601d7d2f0f4e579003d95ae74f9a66c
JackLiR8 <jiakeli1024@gmail.com>
2021-01-12 09:18:18 +0800

    build: modify release.sh, update changelog cli opts

--- scripts/release.sh
+++ scripts/release.sh
@@ -10,7 +10,7 @@ fi
 read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  echo "Releasing $VERSION"
+  echo "Releasing $VERSION ..."
 
   # update version
   npm version $VERSION
@@ -25,4 +25,5 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 
   # push
   git push
+  git push origin refs/tags/v$VERSION
 fi
